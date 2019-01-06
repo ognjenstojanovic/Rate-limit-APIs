26e65631abda7da28021f147d1b7dcda66634c2b
Ognjen Stojanovic <ognjenstojanovic55@gmail.com>
2019-01-06 22:27:52 +0100

    Greet controller with described functionality added.

---
+++ RateLimitAPI/RateLimitAPI/Controllers/GreetController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimitAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("greet")]
+    public class GreetController : Controller
+    {
+        [HttpGet]
+        [Route("{name}")]
+        public IActionResult Get(string name)
+        {
+            return Ok("Hi, " + name);
+        }
+    }
+}
